fb9664fe582a4385171975c84a599b5e65a827e5
quadri salawudeen <olamiquadri1@gmail.com>
2022-08-17 23:21:06 +0100

    animation

--- components/loading.tsx
+++ components/loading.tsx
@@ -4,21 +4,38 @@ import { useRef, useEffect } from "react";
 
 const Loading = () => {
   const Logo: any = useRef();
+  const LogoMove: any = useRef()
+  const LogoImgg: any = useRef();
+  const boxLine: any = useRef();
+  const cover: any = useRef()
 
   useEffect(() => {
-    gsap.to(Logo.current, { y: "-100%" }, 15);
+    gsap.to(boxLine.current, { opacity: 1, duration: 3 }, 2.5);
+    gsap.to(LogoImgg.current, { duration: 3, opacity: 1 }, 4);
+    gsap.to(LogoMove.current, { duration: 3, x: "0%" }, 6);
+    gsap.to(cover.current, { duration: 3, x: "50%" }, 8);
+    gsap.to(Logo.current, { y: "-100%" }, 10.8);
   }, [gsap]);
 
   return (
     <div className={styles.container} ref={Logo}>
-      <div className={styles.box}>
+      <div className={styles.box} ref={boxLine}>
         <div className={styles.dot1}></div>
         <div className={styles.dot2}></div>
         <div className={styles.dot3}></div>
         <div className={styles.dot4}></div>
-
-        <div className={styles.loading}>
-          <img src="/Cri.png" alt="logo" className={styles.logoImg} />{" "}
+      </div>
+      <div className={styles.loading}>
+        <div className={styles.enter} ref={LogoImgg}>
+          <img
+            src="/Cri.png"
+            alt="logo"
+            className={styles.logoImg}
+            ref={LogoMove}
+          />
+        </div>
+        <div className={styles.aniText} >
+          <div className={styles.cover} ref={cover}></div>
           <span>C</span> <span>R</span> <span>I</span> <span>X </span>
         </div>
       </div>
